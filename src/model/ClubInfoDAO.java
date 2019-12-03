1f674a4b57ba50e17855535573c846711416765e
Sakura Yamaguchi <sakura-yamaguchi@isl.im.dendai.ac.jp>
2019-12-03 14:05:16 +0900

    Created Club-related DAO Class

--- src/model/ClubInfoDAO.java
+++ src/model/ClubInfoDAO.java
@@ -1,5 +1,15 @@
 package model;
 
 public class ClubInfoDAO {
+    protected void update(String id,String link,String recogn,String intro,int member,int submember,String icon,String home){
 
+    }
+
+    protected void delete(String id){
+
+    }
+
+    protected ClubInfo getClubInfo(String id){
+
+    }
 }
