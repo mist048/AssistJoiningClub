cca8d8b87d1459b57885a00b43d44ecf803eb30b
mist048 <17fi048@ms.dendai.ac.jp>
2020-01-05 19:37:54 +0900

    管理者問い合わせ用サーブレット追加

--- src/ToContactAdmin.java
+++ src/ToContactAdmin.java
@@ -10,17 +10,17 @@ import javax.servlet.http.HttpServletResponse;
 import tool.PageDataManager;
 
 /**
- * Servlet implementation class FromTop
+ * Servlet implementation class ToContactAdmin
  */
-@WebServlet("/FromTop")
-public class FromTop extends HttpServlet {
+@WebServlet("/ToContactAdmin")
+public class ToContactAdmin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private PageDataManager pageDataManager;
 
 	/**
 	 * @see HttpServlet#HttpServlet()
 	 */
-	public FromTop() {
+	public ToContactAdmin() {
 		super();
 		pageDataManager = PageDataManager.getInstance();
 	}
@@ -42,20 +42,10 @@ public class FromTop extends HttpServlet {
 		String option = request.getParameter("option");
 
 		switch (option) {
-		case "searchResultDisplay": // 検索結果表示画面へ
-			if (!request.getParameter("keyword").equals("")) { // 空欄でなければ
-				pageDataManager.toSearchResultDisplay(request);
-				getServletContext().getRequestDispatcher("/searchResultDisplay.jsp").forward(request, response);
-			} else {
-				getServletContext().getRequestDispatcher("/ToTop").forward(request, response);
-			}
-			break;
-
-		case "clubInfoDisplay": // サークル情報閲覧画面へ
-			pageDataManager.toClubInfoDisplay(request);
-			getServletContext().getRequestDispatcher("/clubInfoDisplay.jsp").forward(request, response);
+		case "contactAdmin": // 管理者問い合わせ画面へ
+			pageDataManager.toContactAdmin(request);
+			getServletContext().getRequestDispatcher("/contactAdmin.jsp").forward(request, response);
 			break;
 		}
 	}
-
 }
